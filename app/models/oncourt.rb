7f5a80b22082351e6ab6ea7207fd5b029b7dbe0b
ZujuSun <z59sun@uwaterloo.ca>
2016-11-24 22:20:57 -0500

    mapping the fields

---
+++ app/models/oncourt.rb
@@ -0,0 +1,5 @@
+class oncourt < ApplicationRecord
+
+
+
+end
---
+++ db/migrate/20161115025952_create_routes.rb
@@ -0,0 +1,10 @@
+class CreateRoutes < ActiveRecord::Migration[5.0]
+  def change
+    create_table :routes do |t|
+      t.string :title
+      t.text :content
+      t.string :image
+      t.timestamps
+    end
+  end
+end
